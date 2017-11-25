3e5729567e3cb020eae387f84670e877aa0e53b1
axgtz <axguti@gmail.com>
2017-11-25 17:44:12 -0600

    removed inverse

--- SFMLFracCompa/alfombraSierpinski.cpp
+++ SFMLFracCompa/alfombraSierpinski.cpp
@@ -10,10 +10,10 @@ SierpinskiCarpet::SierpinskiCarpet(int it, int x, int y){
 SierpinskiCarpet::~SierpinskiCarpet(){}
 
 void SierpinskiCarpet::Render(sf::RenderWindow& window){
-    drawSierpinskiCarpet(sf::Vector2f(0, 0), sf::Vector2f(widthWin, heightWin), 0, window, false, false);
+    drawSierpinskiCarpet(sf::Vector2f(0, 0), sf::Vector2f(widthWin, heightWin), 0, window, false);
 }
 
-void SierpinskiCarpet::drawSierpinskiCarpet(const sf::Vector2f &topLeftPoint, const sf::Vector2f &bottomRightPoint, int currentIteration, sf::RenderWindow &window, bool slow, bool inverse){
+void SierpinskiCarpet::drawSierpinskiCarpet(const sf::Vector2f &topLeftPoint, const sf::Vector2f &bottomRightPoint, int currentIteration, sf::RenderWindow &window, bool slow){
     float width, height;
     int R,G,B;
     if(currentIteration == numIt){
@@ -40,14 +40,8 @@ void SierpinskiCarpet::drawSierpinskiCarpet(const sf::Vector2f &topLeftPoint, co
       return;
     }
     for(int i = 0; i < 9; i++){
-        if(inverse){//Inversa
-            if(i != 4){//Salta todos excepto el 5to cuadro porque es la inversa
-                continue;
-            }
-        }else{//No inversa
-            if(i==4){//Salta el 5to cuadro porque es el vacio
-                continue;//De regreso al principio loop papa
-            }
+        if(i==4){//Salta el 5to cuadro porque es el vacio
+            continue;//De regreso al principio loop papa
         }
         width = std::abs(bottomRightPoint.x - topLeftPoint.x)/3;
         height= std::abs(bottomRightPoint.y - topLeftPoint.y)/3;
@@ -56,6 +50,6 @@ void SierpinskiCarpet::drawSierpinskiCarpet(const sf::Vector2f &topLeftPoint, co
         
         sf::Vector2f newTopLeft = sf::Vector2f(topLeftPoint.x + (ii*width), topLeftPoint.y + ij*height);
         sf::Vector2f newBottomRight = sf::Vector2f(topLeftPoint.x + (ii+1)*width, topLeftPoint.y + (ij+1)*height);
-        drawSierpinskiCarpet(newTopLeft, newBottomRight, currentIteration+1, window, slow, inverse);
+        drawSierpinskiCarpet(newTopLeft, newBottomRight, currentIteration+1, window, slow);
     }
 }
