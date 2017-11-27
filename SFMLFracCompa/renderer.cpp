1ad9b4df98f24163661e91a505b43c6a04edbabc
axgtz <axguti@gmail.com>
2017-11-26 18:27:39 -0600

    new renderer returns shape, slow triangle misses last

--- SFMLFracCompa/renderer.cpp
+++ SFMLFracCompa/renderer.cpp
@@ -7,8 +7,17 @@ void drawTriangle(const sf::Vector2f &top, const sf::Vector2f &left, const sf::V
   shape.setPoint(1, left);
   shape.setPoint(2, right);
   shape.setFillColor(fillColor);
-  //shape.setOutlineColor(fillColor);
-  //shape.setOutlineThickness(2);
   window.draw(shape);
   return;
 }
+
+sf::ConvexShape regresaDibujoTriangulo(const sf::Vector2f &top, const sf::Vector2f &left, const sf::Vector2f &right, const sf::Color &fillColor){
+    sf::ConvexShape shape;
+    shape.setPointCount(3);
+    shape.setPoint(0, top);
+    shape.setPoint(1, left);
+    shape.setPoint(2, right);
+    shape.setFillColor(fillColor);
+    return shape;
+}
+
